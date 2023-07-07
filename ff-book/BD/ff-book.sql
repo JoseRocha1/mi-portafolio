b471e300f20053ae2114d3a722a1d2ea3eba3608
José Saúl Rocha Ruiz <97206631+JoseRocha1@users.noreply.github.com>
2023-07-07 12:16:10 -0600

    Add files via upload

--- ff-book/BD/ff-book.sql
+++ ff-book/BD/ff-book.sql
@@ -1,1 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-05-2022 a las 17:13:08
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 7.3.31
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ff-book`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `libros`
+--
+
+CREATE TABLE `libros` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `editorial` varchar(100) NOT NULL,
+  `facultad` varchar(100) NOT NULL,
+  `carrera` varchar(100) NOT NULL,
+  `semestre` varchar(100) NOT NULL,
+  `enlace` text NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `libros`
+--
+
+INSERT INTO `libros` (`id`, `titulo`, `editorial`, `facultad`, `carrera`, `semestre`, `enlace`, `fecha`) VALUES
+(1, 'Física Moderna', 'Castillo', 'FIME', 'IAS', '4 Semestre', '<a href=\"https://www.academia.edu/11469560/fisica_moderna_norma_esthela_flores_moreno_y_jorge_enrique_figueroa_martinez\">Física Moderna</a>', '2022-05-17 15:46:50'),
+(2, 'Programación Estructurada: Raptor y Lenguaje C', 'Alfa Omega', 'FIME', 'IAS', '2 Semestre', '<a href=\"http://elopez.fime.uanl.mx/@materias/800/Prog.%20basica.pdf\">Programación Estructurada: Raptor y Lenguaje C</a>', '2022-05-17 15:48:46');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `opiniones`
+--
+
+CREATE TABLE `opiniones` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `facultad` varchar(100) NOT NULL,
+  `matricula` varchar(10) NOT NULL,
+  `opinion` text NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `libros`
+--
+ALTER TABLE `libros`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `opiniones`
+--
+ALTER TABLE `opiniones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `libros`
+--
+ALTER TABLE `libros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `opiniones`
+--
+ALTER TABLE `opiniones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
