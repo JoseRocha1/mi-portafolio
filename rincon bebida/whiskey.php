62c659dd7884df05e4edd7d467b4dbbddfdd6650
José Saúl Rocha Ruiz <97206631+JoseRocha1@users.noreply.github.com>
2023-07-07 12:50:53 -0600

    Add files via upload

---
+++ rincon bebida/whiskey.php
@@ -0,0 +1,33 @@
+<?php session_start();
+
+require 'admin/config.php';
+require 'functions.php';
+
+/**
+ * Verifica que haya conexión con la base de datos*/
+$conexion = conexion($bd_config);
+
+if(!$conexion) { //Si no hay conexión
+	header('Location: error.php'); //Me reedirige a la página de error
+}
+
+/**
+ * Verifica que haya una sesion creada*/
+comprobarSessionUsuario();
+
+/**
+ * Obtenienendo los post's */
+$sentencia = $conexion->prepare("SELECT * FROM bebidas WHERE tipo_bebida = 'whiskey'");
+$sentencia->execute();
+
+$post = $sentencia;
+
+/**
+ * Verifica que haya post's*/
+if(!$post) {
+    header('Location: error.php');
+}
+
+require 'views/whiskey.view.php';
+
+?>
