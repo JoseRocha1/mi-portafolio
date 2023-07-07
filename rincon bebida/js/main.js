9944cf1eabc834d2b758081d990ad5c7764eeef0
José Saúl Rocha Ruiz <97206631+JoseRocha1@users.noreply.github.com>
2023-07-07 13:00:01 -0600

    Add files via upload

--- rincon bebida/js/main.js
+++ rincon bebida/js/main.js
@@ -1,1 +1,15 @@
+/* Mostrar y ocultar menú*/
 
+function mostrar() {
+	document.getElementById("sidebar").style.width = "25%";
+	document.getElementById("container").style.marginLeft = "300px";
+	document.getElementById("abrir").style.display = "none";
+	document.getElementById("cerrar").style.display = "inline";
+}
+
+function ocultar() {
+	document.getElementById("sidebar").style.width = "0";
+	document.getElementById("container").style.marginLeft = "0";
+	document.getElementById("abrir").style.display = "inline";
+	document.getElementById("cerrar").style.display = "none";
+}
